0fd904842d5c62ce3e45c503cb503d05cead1a7e
Robin Langlois <langlois.robin@gmail.com>
2023-05-26 12:35:17 +0200

    add pairplot & df city

--- app/app_olympics.py
+++ app/app_olympics.py
@@ -26,7 +26,7 @@ def get_data_sql(query, engine):
 
 #%%
 #read df
-df_athelete = get_data_csv('../data/athlete_events.csv')
+df_athlete = get_data_csv('../data/athlete_events.csv')
 
 df_city_olympics = get_data_csv('../data/olympic_city_country.csv')
 
@@ -34,3 +34,16 @@ st.write(df_city_olympics)
 
 
 st.write(df_athelete)
+
+#%%
+fig = sns.pairplot(df_athelete, hue="Medal")
+
+st.pyplot(fig)
+#%%
+#sub df city for mapping
+
+df_city = df_athlete[['City', 'Year', 'Season']].copy()
+
+df_city = df_city.drop_duplicates().sort_values(by='Year').reset_index(drop=True)
+
+st.write(df_city)
