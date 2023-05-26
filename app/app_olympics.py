aee92d82e23483a1ab9faa711c813e99380ce851
Robin Langlois <langlois.robin@gmail.com>
2023-05-26 14:21:21 +0200

    add locator - lattitude & longitude to cities for mapping

--- app/app_olympics.py
+++ app/app_olympics.py
@@ -8,12 +8,14 @@ import seaborn as sns
 import plotly.express as px
 import os
 import sys
+#from pandas_profiling import ProfileReport
+from geopy.geocoders import Nominatim
 #set path for dynamic function import
 sys.path.append("..")
 dir_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(dir_path)
 
-st.set_page_config(page_title="Olympics Atheltes EDA presentation")
+st.set_page_config(page_title="Olympics Athletes EDA presentation")
 #%%
 #import data
 # @st.cache_data
@@ -25,25 +27,39 @@ def get_data_sql(query, engine):
     return pd.read_sql(query=query, con=engine)
 
 #%%
-#read df
+#reading dfs
 df_athlete = get_data_csv('../data/athlete_events.csv')
 
 df_city_olympics = get_data_csv('../data/olympic_city_country.csv')
 
 st.write(df_city_olympics)
 
-
-st.write(df_athelete)
+st.write(df_athlete)
 
 #%%
-fig = sns.pairplot(df_athelete, hue="Medal")
+#fig = sns.pairplot(df_athlete, hue="Medal")
 
-st.pyplot(fig)
+#st.pyplot(fig)
 #%%
 #sub df city for mapping
 
 df_city = df_athlete[['City', 'Year', 'Season']].copy()
 
+st.write(df_city)
+
+#adding countries to df_city
+df_city = df_city.merge(df_city_olympics, how='left')
+
 df_city = df_city.drop_duplicates().sort_values(by='Year').reset_index(drop=True)
 
 st.write(df_city)
+
+#fetching actual locator from str city name
+locator = Nominatim(user_agent="myGeocoder")
+df_city['loc'] = df_city['City'].apply(lambda x : locator.geocode(x))
+#fetching only lat & long attributes
+df_city['latitude'] = df_city['loc'].apply(lambda x : x.latitude)
+
+df_city['longitude'] = df_city['loc'].apply(lambda x : x.longitude)
+st.write(df_city)
+#df_city.drop_duplicates().sort_values(by='Year').reset_index(drop=True)
