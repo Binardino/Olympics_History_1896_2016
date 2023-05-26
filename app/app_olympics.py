266beef5642ad7d53f733bb38a81cf6d4f3fc63d
Robin Langlois <langlois.robin@gmail.com>
2023-05-26 14:35:22 +0200

    add maps - wip

--- app/app_olympics.py
+++ app/app_olympics.py
@@ -61,5 +61,19 @@ df_city['loc'] = df_city['City'].apply(lambda x : locator.geocode(x))
 df_city['latitude'] = df_city['loc'].apply(lambda x : x.latitude)
 
 df_city['longitude'] = df_city['loc'].apply(lambda x : x.longitude)
+
+df_city.drop('loc', axis=1, inplace=True)
 st.write(df_city)
+
+st.map(df_city)
+
+fig_map = px.scatter_mapbox(df_city, lat="latitude", lon="longitude", zoom=3)
+
+fig_map.update_layout(mapbox_style="open-street-map")
+fig_map.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+st.plotly_chart(fig_map)
+#creating 2 dfs for Summer & Winter Olympics
+df_summer = df_city.loc[(df_city['Season'] == 'Summer')]
+
+df_winter = df_city.loc[(df_city['Season'] == 'Winter')]
 #df_city.drop_duplicates().sort_values(by='Year').reset_index(drop=True)
